faeaa5937c752645219bd724b9080c42a261cfbb
Halil Aydın <hllaydnyzn@gmail.com>
2023-07-01 23:18:45 +0300

    new update

--- directful.py
+++ directful.py
@@ -4,19 +4,6 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-"""img = Image.open("room_image.jpeg")
-st.image(img, caption="room")"""
-
-page_bg_img =
-<style>
-body {
-background-image: url("https://static.leonardo-hotels.com/image/leonardohotelbucharestcitycenter_room_comfortdouble2_2022_4000x2600_7e18f254bc75491965d36cc312e8111f_2048x1331_desktop_2.webp");
-background-size: cover;
-}
-</style>
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 
 def set_background(url):
     if url:
@@ -33,7 +20,7 @@ def set_background(url):
 # Streamlit uygulaması
 def main():
     # Arka plan resminin URL'sini belirleyin
-    background_url = "https://static.leonardo-hotels.com/image/leonardohotelbucharestcitycenter_room_comfortdouble2_2022_4000x2600_7e18f254bc75491965d36cc312e8111f_2048x1331_desktop_2.webp"
+    background_url = "http://www.blog.sagmart.com/wp-content/uploads/2015/08/Best-Hotel-Room-Views.jpg"
     
     # Arka plan resmini ayarlayın
     set_background(background_url)
